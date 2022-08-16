a03f4aa0d2e1f07466e7c944cc2bb228191c2d79
mszerszeniewski <mszerszeniewski@olx.pl>
2022-08-16 12:18:31 +0200

    submission

---
+++ flask/app.py
@@ -0,0 +1,93 @@
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
+
+from model import *
+
+# file upload pattern sourced from https://flask.palletsprojects.com/en/2.0.x/patterns/fileuploads/
+UPLOAD_FOLDER = "static"
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+app = Flask(__name__)
+
+
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["MAX_CONTENT_LENGTH"] = 16 * 1024 * 1024
+
+# source: https: // flask.palletsprojects.com/en/1.1.x/patterns/fileuploads /?highlight = javascript
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            return redirect(url_for("prediction", name=filename))
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    """
+
+
+from flask import send_from_directory
+
+
+@app.route("/predictions/<name>")
+def prediction(name):
+    img_file_path = "/static/" + name
+    detection, breed = dog_breed_predict(img_file_path)
+    if detection == "dog":
+        message = "Dog detected in picture."
+        breed = f"The predicted breed is {breed}."
+    elif detection == "human":
+        message = "Human detected in picture."
+        breed = f"The predicted breed is {breed}."
+    else:
+        message = "No dog nor human detected in picture."
+        breed = "No predicted breed."
+    return render_template(
+        "index.html", path=img_file_path, filename=name, message=message, breed=breed
+    )
+
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        file = request.files["file"]
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            img_file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+
+            # predict function
+            prediction = dog_breed_predict(img_file_path)
+            # delete uploaded file
+            os.remove(img_file_path)
+            return jsonify(prediction)
+        # return True
+
+    return True
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ flask/model.py
@@ -0,0 +1,54 @@
+import pickle
+
+from keras.layers import (
+    Conv2D,
+    Dense,
+    Dropout,
+    Flatten,
+    GlobalAveragePooling2D,
+    MaxPooling2D,
+)
+from keras.models import Sequential
+
+
+def extract_Resnet50(tensor):
+    from keras.applications.resnet50 import ResNet50, preprocess_input
+
+    return ResNet50(weights="imagenet", include_top=False).predict(
+        preprocess_input(tensor)
+    )
+
+
+transfer_model = Sequential()
+transfer_model.add(GlobalAveragePooling2D(input_shape=train_Resnet50.shape[1:]))
+transfer_model.add(Dense(133, activation="softmax"))
+transfer_model.summary()
+transfer_model.load_weights("weights.best.transfer.hdf5")
+
+bottleneck_features = np.load("../bottleneck_features/DogResnet50Data.npz")
+
+
+def Resnet50_predict_breed(img_path):
+    # extract bottleneck features
+    bottleneck_feature = extract_Resnet50(path_to_tensor(img_path))
+    # obtain predicted vector
+    predicted_vector = transfer_model.predict(bottleneck_feature)
+    # return dog breed that is predicted by the model
+    return dog_names[np.argmax(predicted_vector)]
+
+
+def dog_breed_predict(img_path):
+    plt.imshow(cv2.imread(img_path))
+    if dog_detector(img_path) == True:
+        print("Dog detected in image")
+        dog_breed_label = Resnet50_predict_breed(img_path)
+    else:
+        if face_detector(img_path) == True:
+            print("Human detected in image")
+            dog_breed_label = Resnet50_predict_breed(img_path)
+        else:
+            print("No dog or human detected in image")
+            return None
+    return dog_breed_label
+
+
